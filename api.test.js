c48b6fecf749e74ba6e913ef654dc6f1b9d7c2a4
reenesa <susannins@gmail.com>
2019-09-30 01:57:43 +0300

    add menu

---
+++ api.test.js
@@ -0,0 +1,8 @@
+const menu = require('./index');
+const API_KEY = "53b97dacea5e413eb279b06fb97c4122";
+const Base_URL = 'https://newsapi.org/v2/';
+
+
+// test('Returns an error if input is a string', () => {
+//     expect(menu("al-jazeera-english", "us","entertainment").toBe('Invalid input');
+// });
